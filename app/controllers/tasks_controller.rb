79f1735825bbb2df096b5569f7c5c02a2e467196
Vincent Stammbach <vincentsta.dev@gmail.com>
2017-08-08 13:40:49 +0200

    exo 1 ok yc refacto

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -19,18 +20,15 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
     @action = "Edit"
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     redirect_to tasks_path
   end
 
@@ -38,5 +36,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:name, :description, :done)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
